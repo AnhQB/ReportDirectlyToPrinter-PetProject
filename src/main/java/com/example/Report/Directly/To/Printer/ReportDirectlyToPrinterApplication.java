4e194a7ad060c3ba1faa23cad00d8cb1cdd014d2
anhqb <101201273+AnhQB@users.noreply.github.com>
2023-01-02 14:40:01 +0700

    first commit

---
+++ src/main/java/com/example/Report/Directly/To/Printer/ReportDirectlyToPrinterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Report.Directly.To.Printer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportDirectlyToPrinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportDirectlyToPrinterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Report/Directly/To/Printer/ReportDirectlyToPrinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Report.Directly.To.Printer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportDirectlyToPrinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
